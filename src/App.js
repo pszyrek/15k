305e6c4235ee469c222a6ad2e22a3d3106b00f7a
Pawel Bujna <piotrekbujna@github.com>
2017-10-29 19:05:04 +0100

    Delikatna zmiana struktury

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './main.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>is it working?</p>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.scss';
-
-
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <button class="btn btn-primary"> test</button>
-                <h1>DUPA</h1>
-                <p>DUPA</p>
-                <p>DUPA</p>
-            </div>
-        );
-    }
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
